37edb92e5e220b026f533c7ac3a2ce6fd31c72c9
remigiuszkrol <remigiusz.krol93@gmail.com>
2022-04-01 16:01:44 +0200

    ~ REKROL - podstawowy projekt gry Warcaby

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,219 @@ namespace Warcaby
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picGround = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelp1 = new System.Windows.Forms.Label();
+            this.labelp2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Player1 = new System.Windows.Forms.TextBox();
+            this.Player2 = new System.Windows.Forms.TextBox();
+            this.W = new System.Windows.Forms.Panel();
+            this.labelw = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.play = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.W.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picGround
+            // 
+            this.picGround.BackColor = System.Drawing.Color.Gray;
+            this.picGround.Location = new System.Drawing.Point(10, 10);
+            this.picGround.Name = "picGround";
+            this.picGround.Size = new System.Drawing.Size(484, 484);
+            this.picGround.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Warcaby.Properties.Resources.g;
+            this.pictureBox1.Location = new System.Drawing.Point(500, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Warcaby.Properties.Resources.r;
+            this.pictureBox2.Location = new System.Drawing.Point(500, 444);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // labelp1
+            // 
+            this.labelp1.AutoSize = true;
+            this.labelp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelp1.ForeColor = System.Drawing.Color.Green;
+            this.labelp1.Location = new System.Drawing.Point(556, 19);
+            this.labelp1.Name = "labelp1";
+            this.labelp1.Size = new System.Drawing.Size(36, 37);
+            this.labelp1.TabIndex = 3;
+            this.labelp1.Text = "0";
+            // 
+            // labelp2
+            // 
+            this.labelp2.AutoSize = true;
+            this.labelp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelp2.ForeColor = System.Drawing.Color.Red;
+            this.labelp2.Location = new System.Drawing.Point(556, 452);
+            this.labelp2.Name = "labelp2";
+            this.labelp2.Size = new System.Drawing.Size(36, 37);
+            this.labelp2.TabIndex = 4;
+            this.labelp2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(500, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Player 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(500, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player 1";
+            // 
+            // Player1
+            // 
+            this.Player1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.Player1.Location = new System.Drawing.Point(614, 74);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(155, 35);
+            this.Player1.TabIndex = 7;
+            this.Player1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Player1_KeyPress);
+            // 
+            // Player2
+            // 
+            this.Player2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.Player2.Location = new System.Drawing.Point(614, 389);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(155, 35);
+            this.Player2.TabIndex = 8;
+            this.Player2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Player1_KeyPress);
+            // 
+            // W
+            // 
+            this.W.BackColor = System.Drawing.Color.Gold;
+            this.W.Controls.Add(this.panel1);
+            this.W.Controls.Add(this.label4);
+            this.W.Controls.Add(this.labelw);
+            this.W.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.W.Location = new System.Drawing.Point(0, 0);
+            this.W.Name = "W";
+            this.W.Size = new System.Drawing.Size(800, 511);
+            this.W.TabIndex = 9;
+            // 
+            // labelw
+            // 
+            this.labelw.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelw.ForeColor = System.Drawing.Color.White;
+            this.labelw.Location = new System.Drawing.Point(139, 138);
+            this.labelw.Name = "labelw";
+            this.labelw.Size = new System.Drawing.Size(523, 113);
+            this.labelw.TabIndex = 0;
+            this.labelw.Text = "You win";
+            this.labelw.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(246, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(304, 84);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Play again";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.play);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 511);
+            this.panel1.TabIndex = 2;
+            // 
+            // play
+            // 
+            this.play.AutoSize = true;
+            this.play.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.play.ForeColor = System.Drawing.Color.White;
+            this.play.Location = new System.Drawing.Point(320, 208);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(161, 73);
+            this.play.TabIndex = 0;
+            this.play.Text = "Play";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.W);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelp2);
+            this.Controls.Add(this.labelp1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picGround);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Checkers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.W.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel picGround;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelp1;
+        private System.Windows.Forms.Label labelp2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Player1;
+        private System.Windows.Forms.TextBox Player2;
+        private System.Windows.Forms.Panel W;
+        private System.Windows.Forms.Label labelw;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label play;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,244 @@ namespace Warcaby
         {
             InitializeComponent();
         }
+
+        int iSize, green = 0, red = 0;
+        PictureBox[,] picPlate;
+        string color = "g", k = "", B1 = "", B2 = "", k2 = "";
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            W.Visible = false;
+            for (int h = 0; h < iSize; h++)
+                for (int l = 0; l < iSize; l ++)
+                {
+                    if (h < (iSize / 2) - 1 && picPlate[h, l].BackColor == Color.Black) { picPlate[h, l].Image = Properties.Resources.r; picPlate[h, l].Name = h + " " + l + " r"; }
+                    else if (h > (iSize / 2) && picPlate[h, l].BackColor == Color.Black)
+                    {
+                        picPlate[h, l].Image = Properties.Resources.g; picPlate[h, l].Name = h + " " + l + " g";
+                    }
+                    if (h == ((iSize / 2) - 1) || h == (iSize / 2)) picPlate[h, l].Image = null;
+                }
+            labelp1.Text = "0";
+            labelp2.Text = "0";
+            Player1.ReadOnly = false;
+            Player2.ReadOnly = false;
+            Player1.Text = "";
+            Player2.Text = "";
+            Player1.BackColor = Color.White;
+            Player2.BackColor = Color.White;
+            Player1.ForeColor = Color.Black;
+            Player2.ForeColor = Color.Black;
+            red = 0;
+            green = 0;
+        }
+
+        private void Player1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                TextBox t = sender as TextBox;
+                if (t.Text != "" && Player1.Text != Player2.Text)
+                { 
+                    t.ReadOnly = true;
+                    if (t.Name == "Player2")
+                        t.BackColor = Color.Green;
+                    else
+                        t.BackColor = Color.Red;
+                    t.ForeColor = Color.White;
+                }
+                else
+                {
+                    if (t.Text == "")
+                        MessageBox.Show("Player Name can't be blank");
+                    if (Player1.Text == Player2.Text)
+                        MessageBox.Show("The players can't have the same name");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Parent = this;
+            panel1.BringToFront();
+            W.Visible = false;
+            play.Click += (sender2, e2) =>
+            {
+                panel1.Visible = false;
+            };
+            play.MouseHover += (sender3, e3) =>
+            {
+                Label l = sender3 as Label;
+                l.ForeColor = Color.Black;
+                panel1.BackColor = Color.White;
+            };
+            play.MouseLeave += (sender3, e3) =>
+            {
+                Label l = sender3 as Label;
+                l.ForeColor = Color.White;
+                panel1.BackColor = Color.Black;
+            };
+            iSize = 8;
+            picPlate = new PictureBox[iSize, iSize];
+            int iLeft = 2, iTop = 2;
+            Color[] colors = new Color[2];
+            for(int i = 0; i < iSize; i++)
+            {
+                iLeft = 2;
+                if (i % 2 == 0) { colors[0] = Color.White; colors[1] = Color.Black; }
+                else { colors[1] = Color.White; colors[0] = Color.Black; }
+                for (int j = 0; j < iSize; j++)
+                {
+                    picPlate[i, j] = new PictureBox();
+                    picPlate[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+                    picPlate[i, j].Location = new Point(iLeft, iTop);
+                    picPlate[i, j].Size = new Size(60, 60);
+                    iLeft += 60;
+                    picPlate[i, j].Name = i + " " + j;
+                    if (i < (iSize / 2) - 1 && picPlate[i, j].BackColor == Color.Black)
+                    {
+                        picPlate[i, j].Image = Properties.Resources.r;
+                        picPlate[i, j].Name += " r";
+                    }
+                    else if (i > (iSize / 2) && picPlate[i, j].BackColor == Color.Black)
+                    {
+                        picPlate[i, j].Image = Properties.Resources.g;
+                        picPlate[i, j].Name += " g";
+                    }
+                    picPlate[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    picPlate[i, j].MouseHover += (sender2, e2) =>
+                    {
+                        PictureBox pic = sender2 as PictureBox;
+                        if (pic.Image != null) pic.BackColor = Color.FromArgb(255, 64, 64, 64);
+                    };
+                    picPlate[i, j].MouseLeave += (sender2, e2) =>
+                    {
+                        PictureBox pic = sender2 as PictureBox;
+                        if (pic.Image != null) pic.BackColor = Color.Black;
+                    };
+
+                    picPlate[i, j].Click += (sender3, e3) =>
+                    {
+                        if (Player1.ReadOnly && Player2.ReadOnly)
+                        {
+                            PictureBox pic = sender3 as PictureBox;
+                            if (pic.Image != null)
+                            {
+                                int c = -1, x, y;
+                                F();
+
+                                if (pic.Name.Split(' ')[2] == "b")
+                                {
+                                    if (color == "r") color = "g";
+                                    else color = "r";
+                                    x = Convert.ToInt32(k.Split(' ')[0]);
+                                    y = Convert.ToInt32(k.Split(' ')[1]);
+                                    B1 = "";
+                                    B2 = "";
+                                    if (k.Split(' ')[2] == "r")
+                                    {
+                                        pic.Image = Properties.Resources.r;
+                                        pic.Name = pic.Name.Replace("b", "r");
+                                    }
+                                    else if (k.Split(' ')[2] == "g")
+                                    {
+                                        pic.Image = Properties.Resources.g;
+                                        pic.Name = pic.Name.Replace("b", "g");
+                                    }
+                                    picPlate[x, y].Image = null;
+
+                                    if (k2 != "")
+                                    {
+                                        x = Convert.ToInt32(k2.Split(' ')[0]);
+                                        y = Convert.ToInt32(k2.Split(' ')[1]);
+                                        picPlate[x, y].Image = null;
+                                        if (k2.Split(' ')[2] == "r") red++;
+                                        else green++;
+                                        labelp1.Text = green + "";
+                                        labelp2.Text = red + "";
+                                        if (red >= 12)
+                                        {
+                                            labelw.Text = "You win " + Player2.Text;
+                                            W.Visible = true;
+                                        }
+                                        else if (green >= 12)
+                                        {
+                                            labelw.Text = "You win " + Player1.Text;
+                                            W.Visible = true;
+                                        }
+                                        k2 = "";
+                                    }
+
+                                }
+                                else if (pic.Name.Split(' ')[2] == color)
+                                {
+                                    x = Convert.ToInt32(pic.Name.Split(' ')[0]);
+                                    y = Convert.ToInt32(pic.Name.Split(' ')[1]);
+                                    k = pic.Name;
+                                    if (pic.Name.Split(' ')[2] == "r") c = 1;
+                                    try
+                                    {
+                                        if (picPlate[x + c, y + 1].Image == null)
+                                        {
+                                            picPlate[x + c, y + 1].Image = Properties.Resources.b;
+                                            picPlate[x + c, y + 1].Name = (x + c) + " " + (y + 1) + " b";
+                                            B1 = (x + c) + " " + (y + 1);
+                                        }
+                                        else if (picPlate[x + c, y + 1].Name.Split(' ')[2] != pic.Name.Split(' ')[2] && picPlate[x + (c * 2), y + 2].Image == null)
+                                        {
+                                            picPlate[x + (c * 2), y + 2].Image = Properties.Resources.b;
+                                            picPlate[x + (c * 2), y + 2].Name = (x + (c * 2)) + " " + (y + 2) + " b";
+                                            B1 = (x + (c * 2)) + " " + (y + 2);
+                                            k2 = (x + c) + " " + (y + 1) + " " + picPlate[x + c, y + 1].Name.Split(' ')[2];
+                                        }
+                                    }
+                                    catch { }
+
+                                    try
+                                    {
+                                        if (picPlate[x + c, y - 1].Image == null)
+                                        {
+                                            picPlate[x + c, y - 1].Image = Properties.Resources.b;
+                                            picPlate[x + c, y - 1].Name = (x + c) + " " + (y - 1) + " b";
+                                            B2 = (x + c) + " " + (y - 1);
+                                        }
+                                        else if (picPlate[x + c, y - 1].Name.Split(' ')[2] != pic.Name.Split(' ')[2] && picPlate[x + (c * 2), y - 2].Image == null)
+                                        {
+                                            picPlate[x + (c * 2), y - 2].Image = Properties.Resources.b;
+                                            picPlate[x + (c * 2), y - 2].Name = (x + (c * 2)) + " " + (y - 2) + " b";
+                                            B2 = (x + (c * 2)) + " " + (y - 2);
+                                            k2 = (x + c) + " " + (y - 1) + " " + picPlate[x + c, y - 1].Name.Split(' ')[2];
+                                        }
+                                    }
+                                    catch { }
+                                }
+                            }
+                        }
+                    };
+
+                    picGround.Controls.Add(picPlate[i, j]);
+                }
+                iTop += 60;
+            }
+        }
+
+        public void F()
+        {
+            if(B1 != "")
+            {
+                int x, y;
+                x = Convert.ToInt32(B1.Split(' ')[0]);
+                y = Convert.ToInt32(B1.Split(' ')[1]);
+                picPlate[x, y].Image = null;
+            }
+
+            if (B2 != "")
+            {
+                int x, y;
+                x = Convert.ToInt32(B2.Split(' ')[0]);
+                y = Convert.ToInt32(B2.Split(' ')[1]);
+                picPlate[x, y].Image = null;
+            }
+        }
     }
 }
